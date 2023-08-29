bcfc57baabc7f05797a1d56372735f450931f41d
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2023-08-29 10:05:35 +0400

    abstract CLI script

---
+++ src/AbstractCliScript.php
@@ -0,0 +1,132 @@
+<?php
+/*
+ * This file is a part of "charcoal-dev/cli" package.
+ * https://github.com/charcoal-dev/cli
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/charcoal-dev/cli/blob/main/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\CLI;
+
+/**
+ * Class AbstractCliScript
+ * @package Charcoal\CLI
+ */
+abstract class AbstractCliScript
+{
+    /**
+     * @param \Charcoal\CLI\CLI $cli
+     */
+    public function __construct(protected readonly CLI $cli)
+    {
+    }
+
+    /**
+     * @return void
+     */
+    abstract public function exec(): void;
+
+    /**
+     * @param string $line
+     * @param int $sleep
+     * @return $this
+     */
+    final protected function print(string $line, int $sleep = 0): static
+    {
+        $this->cli->print($line, $sleep);
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    final protected function eol(): static
+    {
+        $this->cli->eol();
+        return $this;
+    }
+
+    /**
+     * @param string $line
+     * @param int $sleep
+     * @return $this
+     */
+    final protected function inline(string $line, int $sleep = 0): static
+    {
+        $this->cli->inline($line, $sleep);
+        return $this;
+    }
+
+    /**
+     * @param int $milliseconds
+     */
+    final protected function microSleep(int $milliseconds = 0): void
+    {
+        $this->cli->microSleep($milliseconds);
+    }
+
+    /**
+     * @param string $line
+     * @param int $interval
+     * @param bool $eol
+     * @return $this
+     */
+    final protected function typewriter(string $line, int $interval = 100, bool $eol = false): static
+    {
+        $this->cli->typewriter($line, $interval, $eol);
+        return $this;
+    }
+
+    /**
+     * @param string $char
+     * @param int $count
+     * @param int $interval
+     * @param bool $eol
+     * @return $this
+     */
+    final protected function repeatChar(string $char = ".", int $count = 10, int $interval = 100, bool $eol = false): static
+    {
+        $this->cli->repeatChar($char, $count, $interval, $eol);
+        return $this;
+    }
+
+    /**
+     * @param string $line
+     * @return string|bool
+     */
+    final protected function requireInput(string $line): string|bool
+    {
+        $this->cli->inline(trim($line) . " ");
+        return $this->waitForInput();
+    }
+
+    /**
+     * @return string|bool
+     */
+    final protected function waitForInput(): string|bool
+    {
+        return trim(fgets(STDIN));
+    }
+
+    /**
+     * @return \Charcoal\CLI\Arguments
+     */
+    final protected function args(): Arguments
+    {
+        return $this->cli->args;
+    }
+
+    /**
+     * @return Flags
+     */
+    final protected function flags(): Flags
+    {
+        return $this->cli->flags;
+    }
+}
